1d5b1c2b4d0710a182f62616deff9555072f4dae
Soham Patil <soham1053@gmail.com>
2022-02-08 22:36:57 -0800

    policy gradient, one rollout per batch

--- utils.py
+++ utils.py
@@ -1,17 +1,29 @@
 import matplotlib.pyplot as plt
 import torch
+import numpy as np
 
 
-def plot(values, moving_avg_period):
-    plt.figure(2)
+def plotReturns(values, moving_avg_period):
+    plt.figure(100)
     plt.clf()
-    plt.title('Training...')
     plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(values)
+    plt.ylabel('Return')
+    plt.plot(values, 'blue')
 
     moving_avg = get_moving_average(moving_avg_period, values)
-    plt.plot(moving_avg)
+    plt.plot(moving_avg, 'green')
+    plt.pause(0.001)
+
+
+def plotLosses(values, moving_avg_period):
+    plt.figure(101)
+    plt.clf()
+    plt.xlabel('Episode')
+    plt.ylabel('Loss')
+    plt.plot(values, 'red')
+
+    moving_avg = get_moving_average(moving_avg_period, values)
+    plt.plot(moving_avg, 'orange')
     plt.pause(0.001)
 
 
@@ -23,3 +35,10 @@ def get_moving_average(period, values):
     else:
         moving_avg = torch.zeros(len(values))
     return moving_avg.numpy()
+
+
+# From softmax probabilities, choose index of action
+def probsToIndex(probs_tensor):
+    probs_np = probs_tensor.detach().cpu().numpy()
+    index = np.random.choice(probs_np.size, p=probs_np / probs_np.sum())
+    return index
---
+++ view.py
@@ -0,0 +1,34 @@
+import gym
+from train import Policy
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import time
+from utils import probsToIndex
+
+env = gym.make("CartPole-v1")
+
+input_size = env.observation_space.shape[0]
+output_size = env.action_space.n
+policy = Policy(input_size, output_size)
+policy.load_state_dict(torch.load("policy.pt"))
+policy.eval()
+
+returns = []
+for i in range(10):
+    state = env.reset()
+    returns.append(0)
+    done = False
+    while not done:
+        state = torch.from_numpy(state.astype(np.float32))
+        action_probs = policy(state)
+        action = probsToIndex(action_probs)
+        state, reward, done, _ = env.step(action)
+        env.render()
+        time.sleep(0.1)
+
+        returns[-1] += reward
+env.close()
+
+plt.hist(returns)
+plt.show()
